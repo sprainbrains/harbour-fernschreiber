1e88f3c9ba4605154fb77797267f447f68286f2a
Sebastian Wolf <sebastian@ygriega.de>
2023-11-19 13:34:31 +0100

    Fix chat permissions handling

--- translations/harbour-fernschreiber-es.ts
+++ translations/harbour-fernschreiber-es.ts
@@ -1635,7 +1635,7 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Hide content in Notifications</source>
+        <source>Hide content in notifications</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
